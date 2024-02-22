506b100ca335c61a8a48b22f26e8278fcd92d245
Ravi <rgajare92@gmail.com>
2024-02-22 10:41:41 +0100

    Update autopatcher.py

--- cool_stuff/palworld_server_mods_autopatcher/autopatcher.py
+++ cool_stuff/palworld_server_mods_autopatcher/autopatcher.py
@@ -1,10 +1,10 @@
 import requests
 import ftplib
-
+import pprint
 from credentials import NEXUS_USERNAME, NEXUS_PASSWORD, FTP_SERVER_URL, FTP_USERNAME, FTP_PASSWORD
 
 # Connect to Nexus Mods
-login_url = 'https://www.nexusmods.com/login/auth'
+login_url = 'https://users.nexusmods.com/auth/sign_in'
 session = requests.session()
 
 # Authenticate
@@ -15,6 +15,7 @@ login_data = {
     'stayloggedin': 'false'
 }
 print(session.post(login_url, data=login_data))
+pprint.pprint(session)
 
 # Get mod information
 mod_url = 'https://www.nexusmods.com/mods'
@@ -25,8 +26,8 @@ response = session.get(mod_url)
 # Download updates if available
 
 # Upload files via FTP to gaming server
-ftp = ftplib.FTP(FTP_SERVER_URL, FTP_USERNAME, FTP_PASSWORD)
+# ftp = ftplib.FTP(FTP_SERVER_URL, FTP_USERNAME, FTP_PASSWORD)
 # Perform FTP operations here
 
 # Close FTP connection
-ftp.quit()
+# ftp.quit()
